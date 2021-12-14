5afbe76c2597ca0f338735c10c2d2e99d3c5c4c6
Daniel <danpark105@gmail.com>
2021-12-14 09:13:22 -0500

    navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@chakra-ui/button";
+import { Box, Flex, Link } from "@chakra-ui/layout";
+import NextLink from "next/link";
+import React from "react";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+    // data loading
+  } else if (!data?.me) {
+    // not logged in
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    // logged in
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box bg="tomato" p={4} ml={"auto"}>
+        {body}
+      </Box>
+    </Flex>
+  );
+};
