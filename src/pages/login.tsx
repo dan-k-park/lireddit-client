5fc60f81475cdd0840a5cd7b8e824d00aa95d21f
Daniel <danpark105@gmail.com>
2021-12-15 09:36:58 -0500

    adding email field to users

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -15,9 +15,9 @@ const Login: React.FC<{}> = ({}) => {
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ usernameOrEmail: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await login({ options: values });
+          const response = await login(values);
           if (response.data?.login.errors) {
             // don't need ? after data because ts will infer that it's defined
             // since it's within an if statement
@@ -30,9 +30,9 @@ const Login: React.FC<{}> = ({}) => {
         {({ isSubmitting }) => (
           <Form>
             <InputField
-              name="username"
-              placeholder="username"
-              label="username"
+              name="usernameOrEmail"
+              placeholder="username or email"
+              label="Username or Email"
             />
             <Box mt={4}>
               <InputField
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -17,9 +17,9 @@ const Register: React.FC<registerProps> = ({}) => {
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ email: "", username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
+          const response = await register({ options: values });
           if (response.data?.register.errors) {
             // don't need ? after data because ts will infer that it's defined
             // since it's within an if statement
@@ -37,6 +37,9 @@ const Register: React.FC<registerProps> = ({}) => {
               label="username"
             />
             <Box mt={4}>
+              <InputField name="email" placeholder="email" label="email" />
+            </Box>
+            <Box mt={4}>
               <InputField
                 name="password"
                 placeholder="password"
@@ -44,6 +47,7 @@ const Register: React.FC<registerProps> = ({}) => {
                 type="password"
               />
             </Box>
+
             <Button
               mt={4}
               type="submit"
